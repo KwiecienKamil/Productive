59f67fc15ff243c7865b58edf115bc8518b8efc2
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2024-06-14 21:37:52 +0200

    Day streak component

--- src/app/components/ui/CurrentStreak.tsx
+++ src/app/components/ui/CurrentStreak.tsx
@@ -5,7 +5,7 @@ const CurrentStreak = () => {
     <div className="p-1">
       <p className="text-gray-400">You're on</p>
       <p className="font-bold">
-        <span>5</span> day streak
+        <span>3</span> day streak
       </p>
     </div>
   );
