5deb47e7b6eac758e73c65c45165d1d19aaf7c49
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2024-10-14 21:19:45 +0200

    Daily streak component

--- src/app/components/ui/StreakPresent.tsx
+++ src/app/components/ui/StreakPresent.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import packageImg from "@/app/assets/package.svg";
 import Image from "next/image";
 import { CiCircleCheck } from "react-icons/ci";
+import { toast } from "sonner";
 
 type StreakPresentProps = {
   day: string;
@@ -16,7 +17,12 @@ const StreakPresent = ({
   streak,
   dayNumber,
 }: StreakPresentProps) => {
-  console.log(streak, awardValue);
+  const [awardClaimed, setawardClaimed] = useState(false);
+
+  const handleButtonClick = () => {
+    setawardClaimed(true);
+    toast("XDD");
+  };
 
   return (
     <div className="flex items-center justify-center flex-col gap-2 pt-8">
@@ -24,10 +30,21 @@ const StreakPresent = ({
       <div className="flex items-center flex-col">
         <CiCircleCheck
           className={`text-xl sm:text-3xl ${
-            streak > dayNumber ? "text-green-500" : "gray-400"
+            streak > dayNumber ? "text-green-500" : "text-gray-400"
           }`}
         />
         <span className="text-sm sm:text-md">{day}</span>
+        {streak > dayNumber && !awardClaimed && (
+          <button
+            onClick={handleButtonClick}
+            className="bg-blue-500 text-white rounded px-4 py-2 mt-2 hover:bg-blue-600"
+          >
+            Claim Award
+          </button>
+        )}
+        {awardClaimed && (
+          <span className="text-green-500 text-sm mt-2">Award Claimed!</span>
+        )}
       </div>
     </div>
   );
